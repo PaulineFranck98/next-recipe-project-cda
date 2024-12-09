02c6f55db31e558dfb0f80c1f32fdc0defed1ef6
PaulineFranck98 <pauline-franck@hotmail.fr>
2024-12-09 20:12:22 +0100

    remove any rule from eslint

--- components/FavoriteButton.tsx
+++ components/FavoriteButton.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable  @typescript-eslint/no-explicit-any */
+
 import React from 'react'
 import { useState, useEffect } from 'react'
 import { Heart } from 'lucide-react'
--- components/RecipePDFGenerator.tsx
+++ components/RecipePDFGenerator.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable  @typescript-eslint/no-explicit-any */
+
 import React from 'react'
 import jsPDF from 'jspdf'
 import autoTable, { applyPlugin } from 'jspdf-autotable'
